caa6897171ed138fe56325211a26befc2c435e9b
James <jhlin@nine-yi.com>
2023-12-04 13:06:58 +0800

    Create query API with postgresql

--- Program.cs
+++ Program.cs
@@ -1,6 +1,42 @@
+using dotent_core_min_api.Data;
+using dotnet_core_min_api.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
+
+var conn = builder.Configuration.GetConnectionString("DefaultConnection");
+builder.Services.AddDbContext<MySqlDbContext>(options => options.UseNpgsql(conn));
+
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+app.MapGet("/query", async ([FromQuery] string? sno, MySqlDbContext dbContext) => {
+    Console.WriteLine($"query string: {sno}");
+
+    DeliveryEntity data;
+    if (string.IsNullOrEmpty(sno))
+    {
+        data = await dbContext.delivery.FirstAsync();
+    }
+    else
+    {
+        data = await dbContext.delivery.FirstOrDefaultAsync(i => i.sno == sno);
+    }
+
+    if (data == null)
+    {
+        return new ApiResponseEntity
+        {
+            Status = "error",
+            Error = new { code = 404, message = "Tracking number not found" }
+        };
+    }
+
+    return new ApiResponseEntity
+    {
+        Status = "success",
+        Data = data
+    };
+});
 
 app.Run();
